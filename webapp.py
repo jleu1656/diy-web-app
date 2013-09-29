17b91b1624ad698ff90346d3602e8f808f09a2f0
jleu1656 <jleu1656@gmail.com>
2013-09-29 16:28:34 +1000

    Tester for greeting fn

--- webapp.py
+++ webapp.py
@@ -1,13 +1,12 @@
 from bottle import route, run
-@route('/')
 
+@route('/')
 def index():
-	return "Hello World!"
+    return "Hello World!"
 
 @route('/greet/<name>')
-
 def greet(name):
-	return "Hello, %s!" % name
+    return "Hello, %s!" % name
 
 if __name__ == '__main__':
-	run(debug=True, reloader=True, host='0.0.0.0')
+    run(debug=True, reloader=True, host='0.0.0.0')
