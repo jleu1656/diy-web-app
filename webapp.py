b2bd3ec9b15df3e9227fafbc2e1dcaabab2666ba
jleu1656 <jleu1656@gmail.com>
2013-09-29 16:17:33 +1000

    Add a greeting function

--- webapp.py
+++ webapp.py
@@ -1,7 +1,12 @@
 from bottle import route, run
 @route('/')
+@route('/greet/Jessie')
+
 def index():
 	return "Hello World!"
+	
+def greet(name):
+	return "Hello, %s!" % name
 
 if __name__ == '__main__':
 	run(debug=True, reloader=True, host='0.0.0.0')
